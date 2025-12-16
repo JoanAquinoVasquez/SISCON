10a4e6d3313949eea4633b18cb98b90041a12bc3
Joan Aquino <joan.aquino.vasquez@gmail.com>
2025-12-16 13:43:32 -0500

    add login with firebase

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -7,12 +7,6 @@ use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-    public function __construct()
-    {
-        // Protegemos todo excepto el login
-        $this->middleware('auth:api', ['except' => ['login']]);
-    }
-
     public function login()
     {
         $credentials = request(['email', 'password']);
